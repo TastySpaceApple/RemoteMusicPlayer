9aadca3b3a7d9301606efde8930130a9d814bace
idan <idan.itzhak@gmail.com>
2017-11-20 23:02:16 +0200

    added more options to the led 7segment display module

--- led.js
+++ led.js
@@ -1,17 +1,14 @@
 var SPI = require('spi');
-var spi = new SPI.Spi('/dev/spidev0.0', 
+var spi = new SPI.Spi('/dev/spidev0.0',
 	{'mode': SPI.MODE['MODE_0'],
  	 'bitOrder': false });
 spi.open();
-
-var txbuf = new Buffer([ 0x0F, 0x01 ]);
-var rxbuf = new Buffer([ 0x00, 0x00 ]);
 
 function write(buf){
   var txbuf = new Buffer(buf);
   var rxbuf = new Buffer([0x00, 0x00]);
   spi.transfer(txbuf, rxbuf, function(device, buf) {
-    
+
   })
 }
 
@@ -41,6 +38,48 @@ function setDigit(position, digit, dot){
   console.log(digit)
   write([position + 0x01, digit])
 }
-//write([5 + 0x01, 0x7e])
-setDigit(6, '2', 1);
-module.exports = {init:init, setDigit:setDigit};
+function show(digits){
+	for(var i=0; i<digits.length;i++){
+		setDigit(i+1, buf[i]);
+	}
+}
+function showNumber(number, show_decimal, fit){
+	if(show_decimal == undefined) show_decimal = true;
+	if(fit == undefined) fit = 8;
+	var wholePart = Math.floor(number).toString();
+	var fractionPart = (number - Math.floor(number));
+	var lenWhole = wholePart.length;
+	var lenFraction = fit - lenWhole;
+	fractionPart = Math.floor(fractionPart * Math.pow(10, lenFraction))
+	fractionPart = fractionPart == 0 ? '' : fractionPart.toString();
+  for(var i=fit-1; i>=0; i--){
+		if(i >= lenFraction)
+			setDigit(i+1, wholePart[i - lenFraction], i === lenFraction && show_decimal)
+		else
+			setDigit(i+1, lenFraction[i - lenWhole]);
+	}
+}
+var messageTimer = null;
+function showMessage(message, fit){
+	if(messageTimer != null) throw "Scrolling message already active";
+	if(fit == undefined) fit = 8;
+	message = message.split('');
+	var buffer = [];
+	for(var i=0; i<fit;i++){ buffer.push(0); message.push(''); }
+	var indexInMessage = 0;
+  function interval(){
+		buffer.unshift(message[indexInMessage]).pop();
+		show(buffer);
+		if(++indexInMessage >= message.length){
+			clearInterval(messageTimer);
+			messageTimer = null;
+		}
+	}
+	messageTimer = setInterval(interval, 1000);
+}
+showMessage('123456');
+module.exports = {init: init,
+	setDigit: setDigit,
+	showMessage: showMessage,
+	showNumber: showNumber
+};
